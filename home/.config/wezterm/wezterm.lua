3de16c05a4ac3608c07dfc460d2c39f7b3d3b804
Said <saidazimk11@gmail.com>
2026-07-28 02:34:03 -0600

    Make WezTerm appearance cross-platform

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -6,8 +6,17 @@ config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+
+if wezterm.target_triple:find("windows") then
+  local local_app_data = os.getenv("LOCALAPPDATA")
+  if local_app_data then
+    config.font_dirs = { local_app_data .. "/Microsoft/Windows/Fonts" }
+  end
+  config.win32_system_backdrop = "Acrylic"
+elseif wezterm.target_triple:find("darwin") then
+  config.macos_window_background_blur = 50
+end
 
 return config
